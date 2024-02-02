b0c0768b4e2770a02aea1f06d1d6be3c407f2c19
Lawrence <lawrence891106@gmail.com>
2024-02-02 13:48:20 +0800

    Test"
    wq

---
+++ build/precache-manifest.45c65999322c8793d1c4602636b1ec1a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197bd958e8d97c9efced8df9c03e17af",
+    "url": "/mern-deploy/index.html"
+  },
+  {
+    "revision": "0d8d4a39cdd03720a50c",
+    "url": "/mern-deploy/static/js/2.1f5189bf.chunk.js"
+  },
+  {
+    "revision": "5d439218f41bd20aea31",
+    "url": "/mern-deploy/static/js/main.7373a669.chunk.js"
+  },
+  {
+    "revision": "e7996ece97734914e252",
+    "url": "/mern-deploy/static/js/runtime~main.157b56a9.js"
+  }
+]);
---
+++ src/components/login-component.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthService from "../services/auth.service";
+
+const LoginComponent = (props) => {
+  let { currentUser, setCurrentUser } = props;
+  let navigate = useNavigate();
+  let [email, setEmail] = useState();
+  let [password, setPassword] = useState();
+  let [message, setMessage] = useState();
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleLogin = async () => {
+    try {
+      setMessage("");
+      let response = await AuthService.login(email, password);
+      localStorage.setItem("user", JSON.stringify(response.data));
+      window.alert("登入成功，即將導向至個人頁面");
+      setCurrentUser(AuthService.getCurrentUser());
+      navigate("/profile");
+    } catch (e) {
+      setMessage(e.response.data);
+    }
+  };
+
+  return (
+    <div style={{ padding: "3rem" }} className="col-md-12">
+      <div>
+        {message && <div className="alert alert-danger">{message}</div>}
+        <div className="form-group">
+          <label htmlFor="username">電子信箱：</label>
+          <input
+            onChange={handleChangeEmail}
+            type="text"
+            className="form-control"
+            name="email"
+          />
+        </div>
+        <br />
+        <div className="form-group">
+          <label htmlFor="password">密碼：</label>
+          <input
+            onChange={handleChangePassword}
+            type="password"
+            className="form-control"
+            name="password"
+          />
+        </div>
+        <br />
+        <div className="form-group">
+          <button onClick={handleLogin} className="btn btn-primary btn-block">
+            <span>登入系統</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginComponent;
