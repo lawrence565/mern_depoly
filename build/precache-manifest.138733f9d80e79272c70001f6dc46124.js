104713c8f349c04a56075d9b15ac79f4b4ebf13f
Lawrence <lawrence891106@gmail.com>
2024-02-03 15:17:27 +0800

    2024/02/03

---
+++ build/precache-manifest.138733f9d80e79272c70001f6dc46124.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618eb7a4c89f83476cc3a529712ecfcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d8d4a39cdd03720a50c",
+    "url": "/static/js/2.1f5189bf.chunk.js"
+  },
+  {
+    "revision": "5d439218f41bd20aea31",
+    "url": "/static/js/main.7373a669.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
